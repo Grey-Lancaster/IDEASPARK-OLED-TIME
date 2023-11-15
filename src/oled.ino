0d5887d4110b231a05c02f471a0dd4b6857ca53b
Grey Lancaster <grey@smallbizserver.com>
2023-11-15 18:40:30 -0500

    added day and date

--- src/oled.ino
+++ src/oled.ino
@@ -4,11 +4,11 @@
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
 #include <NTPClient.h>
-#include <Fonts/FreeSans9pt7b.h>  // Include the header file for the custom font
+#include <TimeLib.h>  // Include TimeLib to handle date and time functions
 
 // Replace with your network details
-const char* ssid = "your ap";
-const char* password = "your password";
+const char* ssid = "shop2";
+const char* password = "mine0313";
 
 // OLED display settings
 #define SCREEN_WIDTH 128
@@ -23,9 +23,8 @@ WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "pool.ntp.org", -5 * 3600, 60000); // New York UTC offset -5 hours
 
 void setup() {
-  Serial.begin(115200, SERIAL_8N1);
+  Serial.begin(115200);
 
-  display.setFont(&FreeSans9pt7b);  // Set the custom font
   // Start I2C communication for OLED
   Wire.begin(D6, D5); // GPIO12, GPIO14 respectively
   if (!display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDRESS)) {
@@ -42,57 +41,77 @@ void setup() {
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
-    yield(); // Helps to maintain background tasks
   }
   Serial.println("WiFi connected");
   display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(WHITE);
-  display.setCursor(0,12);
-  display.println("Connected:");//print wi-fi connected on OLED
-  IPAddress theIP = WiFi.localIP();
-  display.setCursor(0,30);
-  display.setTextSize(1);
-  display.println(theIP);//print the ip on the OLED
-  display.display(); // Make sure to add this line to update the display
-  delay(10000);
+  display.setCursor(0,0);
+  display.println("Wi-Fi Connected");
+  display.display();
+  delay(2000); // Show the message for 2 seconds
 
   // Start the NTP client
   timeClient.begin();
   // Uncomment and adjust the following line for daylight saving time if necessary
-  // timeClient.setTimeOffset(-4 * 3600); // EDT offset
+  // timeClient.setTimeOffset(-4 * 3600); // EDT offset for daylight saving time
 }
 
 void loop() {
   timeClient.update();
+  unsigned long epochTime = timeClient.getEpochTime();
 
-  // Get the current hour, minute and second from the NTP client
-  int currentHour = timeClient.getHours();
+  // Parse the NTP time to extract date and time components
+  int currentHour = hour(epochTime);
+  int currentMinute = minute(epochTime);
+  int currentSecond = second(epochTime);
+  int dayOfWeek = weekday(epochTime);
+  int currentDay = day(epochTime);
+  int currentMonth = month(epochTime);
+  int currentYear = year(epochTime);
+
+  // Convert to 12-hour format and set AM/PM
   String am_pm = "AM";
-  
   if (currentHour >= 12) {
     am_pm = "PM";
-    if (currentHour > 12) currentHour -= 12; // Convert to 12-hour format
+    if (currentHour > 12) currentHour -= 12;
   }
-  if (currentHour == 0) {
-    currentHour = 12; // Midnight is shown as 12AM
-  }
+  if (currentHour == 0) currentHour = 12;
 
-  // Construct the time string
-  String currentTime = String(currentHour < 10 ? "0" : "") + String(currentHour) + ":" +
-                       String(timeClient.getMinutes() < 10 ? "0" : "") + String(timeClient.getMinutes()) + ":" +
-                       String(timeClient.getSeconds() < 10 ? "0" : "") + String(timeClient.getSeconds()) + " " + am_pm;
-
-  // Display the time on the OLED
+  // Clear the display buffer
   display.clearDisplay();
+
+  // Display time
   display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.setCursor(0,12);
-  display.println("The Time is:");
-  display.setTextSize(1);
-  display.setCursor(0, 30);
-  display.println(currentTime);
+  display.setCursor(0,0);
+  display.print("Time: ");
+  if (currentHour < 10) display.print('0');
+  display.print(currentHour);
+  display.print(":");
+  if (currentMinute < 10) display.print('0');
+  display.print(currentMinute);
+  display.print(":");
+  if (currentSecond < 10) display.print('0');
+  display.print(currentSecond);
+  display.print(" ");
+  display.println(am_pm);
+
+  // Display the day of the week
+  display.print("Day: ");
+  display.println(dayStr(dayOfWeek));
+
+  // Display the date in MM/DD/YYYY format
+  display.print("Date: ");
+  if (currentMonth < 10) display.print('0');
+  display.print(currentMonth);
+  display.print("/");
+  if (currentDay < 10) display.print('0');
+  display.print(currentDay);
+  display.print("/");
+  display.println(currentYear);
+
+  // Send the buffer to the display
   display.display();
 
-  delay(1000);
+  delay(1000); // Wait for a second before looping again
 }
